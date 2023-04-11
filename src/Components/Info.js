7f0ba97487c45b1998ac5a42c2c10338b9c6fda6
Komal Meena <21meenakk@gmail.com>
2023-04-11 13:45:30 +0530

    first

---
+++ src/Components/Info.js
@@ -0,0 +1,15 @@
+
+
+import "../style.css"
+
+const Info = () => (
+    <div className="info">
+<h1>Skill</h1>
+<p>Stars are the most widely recognized astronomical objects, and represent the most fundamental building blocks of galaxies. The age, distribution, and composition of the stars in a galaxy trace the history, dynamics, and evolution of that galaxy. Moreover, stars are responsible for the manufacture and distribution of heavy elements such as carbon, nitrogen, and oxygen, and their characteristics are intimately tied to the characteristics of the planetary systems that may coalesce about them. Consequently, the study of the birth, life, and death of stars is central to the field of astronomy. <br></br>
+Stars are the most widely recognized astronomical objects, and represent the most fundamental building blocks of galaxies. The age, distribution, and composition of the stars in a galaxy trace the history, dynamics, and evolution of that galaxy. Moreover.
+ stars are responsible for the manufacture and distribution of heavy elements such as carbon, nitrogen, and oxygen, and their characteristics are intimately tied to the characteristics of the planetary systems that may coalesce about them. Consequently, the study of the birth, life, and death of stars is central to the field of astronomy.
+</p>
+    </div>
+)
+
+export default Info;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+
+import "../style.css";
+const Navbar = () =>{
+     return(
+        <nav className="navbar">
+            <h1>Komal Meena</h1>
+            <div className="links">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Skills</a>
+                <a href="#">Projects</a>
+                <a href="#">Blogs</a>
+            </div>
+        </nav>
+     )
+}
+
+export default Navbar;
